3ed76d0e0724e600975e857803a5f39c96694137
squal <squalporeover.ju@outlook.com>
2019-03-30 01:16:16 +0600

    TabularInline for ProductImage and Variation model at the product add page in the admin model

--- products/admin.py
+++ products/admin.py
@@ -1,8 +1,24 @@
 from django.contrib import admin
 from .models import Product, Variation, ProductImage, Category, ProductFeatured
 
+
+# In the admin product add page Variation and productImge model shown vertically
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+class VariationInline(admin.TabularInline):
+    model = Variation
+    extra = 0
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'active', 'default']
+    inlines = [ProductImageInline, VariationInline]
+    class Meta:
+        model = Product
+
 # Register your models here.
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
 admin.site.register(Variation)
 admin.site.register(ProductImage)
 admin.site.register(Category)
